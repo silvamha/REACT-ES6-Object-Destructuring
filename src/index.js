a903246737400dc9c4e45db96abd81be8079906f
Ed <silva.email@yahoo.com>
2024-03-03 20:31:01 -0500

    Refactor table structure in index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
-import { hondaModel, hondaTopColour, hondaTopSpeed, teslaModel, teslaTopColour, teslaTopSpeed } from "./practice";
+import {
+  hondaModel,
+  hondaTopColour,
+  hondaTopSpeed,
+  teslaModel,
+  teslaTopColour,
+  teslaTopSpeed,
+} from "./practice";
 
 ReactDOM.render(
-    <table>
-        <tr>
-            <th>Brand</th>
-            <th>Top Speed</th>
-            <th>Top Colour</th>
-        </tr>
-        <tr>
-            <td>{teslaModel}</td>
-            <td>{teslaTopSpeed}</td>
-            <td>{teslaTopColour}</td>
-        </tr>
-        <tr>
-            <td>{hondaModel}</td>
-            <td>{hondaTopSpeed}</td>
-            <td>{hondaTopColour}</td>
-        </tr>
-    </table>,
-    document.getElementById("root")
+
+  <tbody>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+        <th>Top Colour</th>
+      </tr>
+      <tr>
+        <td>{teslaModel}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{hondaModel}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+  </tbody>,
+  document.getElementById("root")
 );
